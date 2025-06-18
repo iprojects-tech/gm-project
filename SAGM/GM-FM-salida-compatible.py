512de2f28a4a7c39d4f7ca944b6b6cd44cef4475
iprojects-tech <pepeorgu.ty@gmail.com>
2025-06-17 22:31:05 -0600

    SAGM Update

--- SAGM/GM-FM-salida-compatible.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:17346dfe504496d16c2ccc96336deacaeb94613c0daf0b850283ca5ef149563c
-size 6508
--- SAGM/app.py
+++ SAGM/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f9b556fdc1157dc5b38af418d5976392c323f0dc53c0eb0c0ee1f39c62f73a4
-size 770
+oid sha256:e371a8f3b7adc1ef51dc91449c9dab799e76c81f68a798d656417c477815d666
+size 657
